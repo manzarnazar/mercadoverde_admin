fa68fe473ba96a510b9a06e58e7ab825a86fdbdb
Abdul Hadi <manzarnazar@gmail.com>
2026-06-17 10:53:18 +0500

    shaping tool issue

--- resources/views/admin-views/zone/edit.blade.php
+++ resources/views/admin-views/zone/edit.blade.php
@@ -141,7 +141,8 @@
 @endsection
 
 @push('script_2')
-<script src="{{ asset('public/assets/admin/js/view-pages/zone-map-drawing.js') }}"></script>
+{{-- zone-map-v2 --}}
+@include('admin-views.zone.partials._zone-map-drawing-script')
 <script async defer
     src="https://maps.googleapis.com/maps/api/js?key={{\App\Models\BusinessSetting::where('key', 'map_api_key')->first()->value}}&callback=initialize&libraries=places&loading=async"></script>
 <script>
